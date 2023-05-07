52a69481c878445d8fd4b6d9b3ad6f5d648184e6
Iván Rodríguez <ivan18rt@outlook.com>
2023-05-07 17:32:46 -0600

    proyecto listo

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ let editID = "";
 //submit form
 form.addEventListener("submit", addItem);
 clearBtn.addEventListener("click", clearItems);
+window.addEventListener("DOMContentLoaded", setupItems);
 
 // ****** FUNCTIONS **********
 function addItem(e) {
@@ -27,32 +28,9 @@ function addItem(e) {
 
   //Si se inserta
   if (groceryValue && !editFlag) {
-    //Crea un elemento nuevo
-    const element = document.createElement("article");
-    element.classList.add("grocery-item"); //Se agrega la clase
-    //Crea el atributo data-id con id unico
-    const attr = document.createAttribute("data-id");
-    attr.value = id;
-    element.setAttributeNode(attr); //Enlaza el atributo
-    //Crea el elemento
-    element.innerHTML = `<p class="title">${groceryValue}</p>
-          <div class="btn-container">
-            <button type="button" class="edit-btn">
-              <i class="fas fa-edit"></i>
-            </button>
-            <button type="button" class="delete-btn">
-              <i class="fas fa-trash"></i>
-            </button>
-          </div>`;
-    const editBtn = element.querySelector(".edit-btn");
-    const deleteBtn = element.querySelector(".delete-btn");
-    editBtn.addEventListener("click", editItem);
-    deleteBtn.addEventListener("click", deleteItem);
-
-    //Se agrega a la lista
-    list.appendChild(element);
+    createListItem(id, groceryValue);
     //Muestra alerta de exito
-    displayAlert("Item added successfully", "success");
+    displayAlert("Se ha guardado el item", "success");
     container.classList.add("show-container"); //Se muestra el contenedor que estaba oculto
 
     //Agrega a localStorage
@@ -61,7 +39,10 @@ function addItem(e) {
     setBackToDefault();
   } else if (groceryValue && editFlag) {
     //Si se edita
-    console.log("Editing");
+    editElement.textContent = groceryValue;
+    displayAlert("Se ha editado el item", "success");
+    editLocalStorage(editID, groceryValue);
+    setBackToDefault();
   } else {
     //Si no se ha introducido nada
     displayAlert("No se ha agregado ningun item", "danger");
@@ -69,13 +50,26 @@ function addItem(e) {
 }
 
 //Edita un elemento
-function editItem() {
-  console.log("edit");
+function editItem(e) {
+  const element = e.currentTarget.parentNode.parentNode;
+  editElement = e.currentTarget.parentElement.previousElementSibling;
+  grocery.value = editElement.textContent;
+  editFlag = true;
+  editID = element.dataset.id;
+  submitBtn.textContent = "Editar";
 }
 
 //Elimina un elemento
-function deleteItem() {
-  console.log("delete");
+function deleteItem(e) {
+  const element = e.currentTarget.parentNode.parentNode;
+  const id = element.dataset.id;
+  list.removeChild(element);
+  if (list.children.length === 0) {
+    container.classList.remove("show-container");
+  }
+  displayAlert("Item borrado de la lista", "danger");
+  setBackToDefault();
+  removeFromStorage(id);
 }
 
 //Elimina todos los elementos de la lista
@@ -90,7 +84,8 @@ function clearItems() {
   container.classList.remove("show-container");
   displayAlert("Se ha limpiado la lista", "success");
   setBackToDefault();
-  // localStorage.remove(list);
+  // Elimina de localStorage
+  localStorage.removeItem("list");
 }
 
 //Funcion que muestra una alerta
@@ -110,12 +105,85 @@ function setBackToDefault() {
   grocery.value = "";
   editFlag = false;
   editID = "";
-  submitBtn.textContent = "Submit";
+  submitBtn.textContent = "Guardar";
 }
 
 // ****** LOCAL STORAGE **********
 function addToLocalStorage(id, value) {
-  console.log("Added to local storage");
+  //Objeto a guardar
+  const grocery = { id, value };
+  //Si hay elementos en lista los extrae, si no se guarda como un arreglo vacio provisional
+  let items = getLocalStorage();
+  //Lo inserta en items
+  items.push(grocery);
+  //Lo guarda en locaStorage
+  localStorage.setItem("list", JSON.stringify(items));
+}
+
+function editLocalStorage(id, value) {
+  //Obtiene los elementos del localStorage
+  let items = getLocalStorage();
+  //Cambia el valor al elemento usando el id para buscarlo
+  const item = items.map(function (item) {
+    if (item.id === id) {
+      item.value = value;
+    }
+    return item;
+  });
+  //Lo guarda en locaStorage
+  localStorage.setItem("list", JSON.stringify(items));
+}
+
+function removeFromStorage(id) {
+  let items = getLocalStorage();
+  items = items.filter(function (item) {
+    if (item.id !== id) {
+      return item;
+    }
+  });
+  localStorage.setItem("list", JSON.stringify(items));
+}
+
+function getLocalStorage() {
+  return localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
 }
 
 // ****** SETUP ITEMS **********
+function setupItems() {
+  let items = getLocalStorage();
+  if (items.length > 0) {
+    items.forEach(function (item) {
+      createListItem(item.id, item.value);
+    });
+    container.classList.add("show-container"); //Se muestra el contenedor que estaba oculto
+  }
+}
+
+function createListItem(id, groceryValue) {
+  //Crea un elemento nuevo
+  const element = document.createElement("article");
+  element.classList.add("grocery-item"); //Se agrega la clase
+  //Crea el atributo data-id con id unico
+  const attr = document.createAttribute("data-id");
+  attr.value = id;
+  element.setAttributeNode(attr); //Enlaza el atributo
+  //Crea el elemento
+  element.innerHTML = `<p class="title">${groceryValue}</p>
+          <div class="btn-container">
+            <button type="button" class="edit-btn">
+              <i class="fas fa-edit"></i>
+            </button>
+            <button type="button" class="delete-btn">
+              <i class="fas fa-trash"></i>
+            </button>
+          </div>`;
+  const editBtn = element.querySelector(".edit-btn");
+  const deleteBtn = element.querySelector(".delete-btn");
+  editBtn.addEventListener("click", editItem);
+  deleteBtn.addEventListener("click", deleteItem);
+
+  //Se agrega a la lista
+  list.appendChild(element);
+}
